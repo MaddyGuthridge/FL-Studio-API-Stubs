6004f9f85d65507e197100c4d2182b71fe8d0952
Miguel Guthridge <hdsq@outlook.com.au>
2021-09-08 16:07:33 +1000

    ✨NEW: More functions in ui module

--- src/ui.py
+++ src/ui.py
@@ -151,3 +151,246 @@ def right(value:int=1) -> int:
     Returns:
      * `int`: ?
     """
+
+def horZoom(value: int) -> int:
+    """Zoom horizontally by `value`.
+
+    Args:
+     * `value` (`int`): amount to zoom by. Negative zooms out, positive zooms in.
+        Larger magnitudes zoom more, but the scale doesn't seem consistent.
+
+    Returns:
+     * `int`: ?
+    """
+
+def verZoom(value: int) -> int:
+    """Zoom vertically by `value`.
+
+    Args:
+     * `value` (`int`): amount to zoom by. Negative zooms out, positive zooms in.
+        Larger magnitudes zoom more, but the scale doesn't seem consistent.
+
+    Returns:
+     * `int`: ?
+    """
+
+def snapOnOff() -> int:
+    """Toggle whether snapping is enabled globally.
+
+    Returns:
+     * `int`: ?
+    """
+
+def cut() -> int:
+    """Cut the selection.
+    
+    WARNING: This function echos the hotkey to cut, and thus will affect
+    programs outside of FL Studio. Use with caution.
+
+    Returns:
+     * `int`: ?
+    """
+
+def copy() -> int:
+    """Copy the selection.
+    
+    WARNING: This function echos the hotkey to copy, and thus will affect
+    programs outside of FL Studio. Use with caution.
+
+    Returns:
+     * `int`: ?
+    """
+
+def paste() -> int:
+    """Paste the selection.
+    
+    WARNING: This function echos the hotkey to paste, and thus will affect
+    programs outside of FL Studio. Use with caution.
+
+    Returns:
+     * `int`: ?
+    """
+
+def insert() -> int:
+    """Press the insert key.
+    
+    WARNING: This function echos the insert key, and thus will affect
+    programs outside of FL Studio. Use with caution.
+
+    Returns:
+     * `int`: ?
+    """
+
+def delete() -> int:
+    """Press the delete key.
+    
+    WARNING: This function echos the delete key, and thus will affect
+    programs outside of FL Studio. Use with caution.
+
+    Returns:
+     * `int`: ?
+    """
+
+def enter() -> int:
+    """Press the enter key.
+    
+    WARNING: This function echos the enter key, and thus will affect
+    programs outside of FL Studio. Use with caution.
+
+    Returns:
+     * `int`: ?
+    """
+
+def escape() -> int:
+    """Press the escape key.
+    
+    WARNING: This function echos the escape key, and thus will affect
+    programs outside of FL Studio. Use with caution.
+
+    Returns:
+     * `int`: ?
+    """
+
+def yes() -> int:
+    """Press the y key.
+    
+    WARNING: This function echos the y key, and thus will affect
+    programs outside of FL Studio. Use with caution.
+
+    Returns:
+     * `int`: ?
+    """
+
+def no() -> int:
+    """Press the n key.
+    
+    WARNING: This function echos the n key, and thus will affect
+    programs outside of FL Studio. Use with caution.
+
+    NOTE: This function is listed in the official documentation as `not`, 
+    however this is incorrect, and will result in a syntax error since
+    overriding core keywords (such as `if`, `def` and `not`) is not allowed. The
+    function is actually named `no`, which is how this documentation lists it.
+
+    Returns:
+     * `int`: ?
+    """
+
+def getHintMsg() -> str:
+    """Returns the current message in FL Studio's hint panel.
+
+    Returns:
+     * `str`: hint
+    """
+
+def setHintMessage(msg: str) -> None:
+    """Sets the current hint message in FL Studio's hint panel to `msg`.
+
+    Args:
+     * `msg` (`str`): new message
+    """
+
+def getHintValue(value: int, max: int) -> str:
+    """Returns hint for `value`.
+    
+    HELP WANTED: What does this do?
+
+    Args:
+     * `value` (`int`): ???
+     * `max` (`int`): ???
+
+    Returns:
+     * `str`: hint for `value`
+    """
+
+def getTimeDispMin() -> bool:
+    """Returns `True` when the song position panel is displaying time, rather
+    than bar and beat.
+
+    Returns:
+     * `bool`: whether song position is displaying time.
+    """
+
+def setTimeDispMin() -> None:
+    """Toggles whether the song position panel is displaying time or bar and 
+    beat.
+    """
+
+def getVisible(index: int) -> bool:
+    """Returns whether an FL Studio window is visible.
+
+    Args:
+     * `index` (`int`): window index:
+            * `widMixer` (`0`): Mixer
+            * `widChannelRack` (`1`): Channel Rack
+            * `widPlaylist` (`2`): Playlist
+            * `widPianoRoll` (`3`): Piano Roll
+            * `widBrowser` (`4`): Browser
+
+    Returns:
+     * `bool`: whether it is visible
+    """
+
+def showWindow(index: int) -> None:
+    """Shows an FL Studio window specified by `index`.
+
+    Args:
+     * `index` (`int`): window index:
+            * `widMixer` (`0`): Mixer
+            * `widChannelRack` (`1`): Channel Rack
+            * `widPlaylist` (`2`): Playlist
+            * `widPianoRoll` (`3`): Piano Roll
+            * `widBrowser` (`4`): Browser
+    """
+
+def hideWindow(index: int) -> None:
+    """Hides an FL Studio window specified by `index`.
+
+    Args:
+     * `index` (`int`): window index:
+            * `widMixer` (`0`): Mixer
+            * `widChannelRack` (`1`): Channel Rack
+            * `widPlaylist` (`2`): Playlist
+            * `widPianoRoll` (`3`): Piano Roll
+            * `widBrowser` (`4`): Browser
+    """
+
+def getFocused(index: int) -> bool:
+    """Returns whether an FL Studio window is focused (meaning it is the
+    currently selected Window in FL Studio). 
+    
+    NOTE: this doesn't necessarily mean that it is the currently selected window
+    in the host operating system, so functions that rely on keypress emulation 
+    (such as `ui.copy()`) may not work as intended, even if this returns `True`.
+    
+    Args:
+     * `index` (`int`): window index:
+            * `widMixer` (`0`): Mixer
+            * `widChannelRack` (`1`): Channel Rack
+            * `widPlaylist` (`2`): Playlist
+            * `widPianoRoll` (`3`): Piano Roll
+            * `widBrowser` (`4`): Browser
+            * `widPlugin` (`5`): Plugin Window (note that this constant is only
+              usable in this particular function).
+
+    Returns:
+     * `bool`: whether it is visible
+    """
+
+def setFocused(index: int) -> Note:
+    """Sets which FL Studio window should be focused (meaning it is the
+    currently selected Window in FL Studio). 
+    
+    NOTE: this doesn't necessarily mean that it will be the currently selected 
+    window in the host operating system, so functions that rely on keypress 
+    emulation (such as `ui.copy()`) may not work as intended, even after calling
+    this function.
+    
+    Args:
+     * `index` (`int`): window index:
+            * `widMixer` (`0`): Mixer
+            * `widChannelRack` (`1`): Channel Rack
+            * `widPlaylist` (`2`): Playlist
+            * `widPianoRoll` (`3`): Piano Roll
+            * `widBrowser` (`4`): Browser
+    """
