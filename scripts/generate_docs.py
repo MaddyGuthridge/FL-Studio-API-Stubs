f36bc0b281c0b9f4f094cad8d1734dd90336b575
Miguel Guthridge <hdsq@outlook.com.au>
2022-07-07 01:06:08 +1000

    Add basic template for landing page

--- scripts/generate_docs.py
+++ scripts/generate_docs.py
@@ -1,8 +1,14 @@
+import sys
 from pathlib import Path
 import shutil
 from pdoc import pdoc, render  # type: ignore
+from pdoc.web import DocServer, open_browser
 
-MODULES = [
+# Add source module to path
+sys.path.insert(0, './src')
+import general  # noqa: E402
+
+modules = [
     'arrangement',
     'channels',
     'device',
@@ -28,13 +34,38 @@ except FileNotFoundError:
 
 render.configure(
     docformat='markdown',
-    footer_text='FL Studio Python API Stubs',
+    footer_text=f'FL Studio Python API Stubs (v{general.getVersion()})',
     show_source=False,
+    template_directory=Path('./resources/pdoc_templates'),
 )
 
-print("Generating...")
-pdoc(
-    *['src/' + m for m in MODULES],
-    output_directory=Path('./pdoc')
-)
-print("Done!")
+modules = ['src/' + m for m in modules]
+
+# Generate the docs
+if len(sys.argv) == 1:
+    print('Generating...')
+    pdoc(
+        *modules,
+        output_directory=Path('./pdoc')
+    )
+    print('Done!')
+
+# Start a web server
+elif sys.argv[1] == 'server':
+    server = DocServer(('localhost', 8080), modules)
+    with server:
+        url = f'http://localhost:{server.server_port}'
+        print(f'pdoc server ready at {url}')
+        open_browser(url)
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print('\nGoodbye!')
+            server.server_close()
+
+# Invalid args
+else:
+    print(
+        f'Error: unrecognized argument {sys.argv[1]} (available: `server`)',
+        file=sys.stderr
+    )
