53c4e63ffff1f4a78459cf49e140a548e2ac4d12
Miguel Guthridge <hdsq@outlook.com.au>
2022-09-13 15:56:38 +1000

    Disable linter on midi module

--- src/midi/__init__.py
+++ src/midi/__init__.py
@@ -7,6 +7,7 @@ NOTE: This code is taken from FL Studio's Python lib folder and included in
 this package in the hope that it will be useful for script developers. It is
 not the creation of the repository authors, and no credit is claimed.
 """
+# flake8: noqa
 
 MaxInt = 2147483647
 
