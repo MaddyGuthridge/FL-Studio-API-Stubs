2bbf1c3e2a4f05e77af4b8a18ae6b80f3110520f
abbydiode <contact@abbydiode.com>
2022-12-17 22:19:07 +0100

    Delete pdoc specific files

--- scripts/coverage.sh
+++
@@ -1,5 +0,0 @@
-# Gather code coverage display results in a web browser
-coverage run -m pytest \
-&& coverage report \
-&& coverage html \
-&& python -m webbrowser htmlcov/index.html > /dev/null 2>&1
--- scripts/update.sh
+++
@@ -1,7 +0,0 @@
-# Push an update to Pypi
-
-rm -rf ./dist
-
-python -m build
-
-python -m twine upload dist/*
