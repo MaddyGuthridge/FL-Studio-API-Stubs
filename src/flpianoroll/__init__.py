87b2c8dceb6ab94387a2591a8d300337533913c9
Miguel Guthridge <hdsq@outlook.com.au>
2023-07-21 02:41:32 +1000

    Add flpianoroll module

---
+++ src/flpianoroll/__init__.py
@@ -0,0 +1,12 @@
+"""
+# FL Studio Piano Roll
+
+This module provides editing tools for interacting with notes in the
+piano roll, using its integrated scripts functionality.
+
+Main script files should use the `.pyscript` file extension, with additional
+modules using standard `.py` files.
+
+Note that this module is not accessible in MIDI Controller Scripts, it can only
+be used in scripts that run in the piano roll
+"""
