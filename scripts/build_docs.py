a365c49dad91f6535e01c4de9afa6e6681bce68e
Miguel Guthridge <hdsq@outlook.com.au>
2024-01-09 18:12:16 +1100

    Add poetry script for building docs

--- scripts/build_docs.py
+++ scripts/build_docs.py
@@ -3,11 +3,11 @@ from mkdocs.commands.build import build as mkdocs_build
 from mkdocs.config import load_config
 
 
-def build():
+def main():
     generate()
     config = load_config()
     mkdocs_build(config)
 
 
 if __name__ == "__main__":
-    build()
+    main()
