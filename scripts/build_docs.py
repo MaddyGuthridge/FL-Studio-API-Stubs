7ab2f42b9c245b43f37ca742e26befca0c3b6602
Miguel Guthridge <hello@miguelguthridge.com>
2024-01-14 22:13:03 +1100

    Improve docs build process to reduce time `site` directory is missing for (improving live reload)

--- scripts/build_docs.py
+++ scripts/build_docs.py
@@ -1,12 +1,18 @@
 from .generate_pages import generate
 from mkdocs.commands.build import build as mkdocs_build
 from mkdocs.config import load_config
+from pathlib import Path
+from shutil import rmtree, move
 
 
 def main():
     generate()
     config = load_config()
     mkdocs_build(config)
+    temp_dir = Path('temp_site')
+    assert temp_dir.is_dir()
+    rmtree('site', ignore_errors=True)
+    move(temp_dir, 'site')
 
 
 if __name__ == "__main__":
